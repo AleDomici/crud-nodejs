175fec9ac55858e4465ba1f862817f78c7b439ad
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-07-16 14:35:12 -0300

    feat(java): cria entidade Produto com campos id, nome, dataCriacao e quantidadeDisponivel
    Adiciona a classe Produto em crudnodejs.com.br.model.
    Inclui atributos id (String), nome (String), dataCriacao (String) e quantidadeDisponivel (Integer),
    além de getters e setters para cada campo.

---
+++ src/main/java/crudnodejs/com/br/model/Produto.java
@@ -0,0 +1,21 @@
+package crudnodejs.com.br.model;
+
+public class Produto {
+    private String id;
+    private String nome;
+    private String dataCriacao;
+    private Integer quantidadeDisponivel;
+
+    // Getters e Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDataCriacao() { return dataCriacao; }
+    public void setDataCriacao(String dataCriacao) { this.dataCriacao = dataCriacao; }
+
+    public Integer getQuantidadeDisponivel() { return quantidadeDisponivel; }
+    public void setQuantidadeDisponivel(Integer quantidadeDisponivel) { this.quantidadeDisponivel = quantidadeDisponivel; }
+}
