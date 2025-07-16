af2b98da77763d9a60f323b14664f76a16f0ad97
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-07-16 14:35:23 -0300

    feat(java): adiciona ProdutoDTO para transferência de dados
    Cria a classe ProdutoDTO em crudnodejs.com.br.dtos,
    com os campos id (Long) e nome (String), incluindo getters e setters.
    Facilita a comunicação entre as camadas da aplicação.

---
+++ src/main/java/crudnodejs/com/br/dtos/ProdutoDTO.java
@@ -0,0 +1,14 @@
+// src/main/java/crudnodejs.com.br.controllers/ProdutoDTO.java
+package crudnodejs.com.br.dtos;
+
+public class ProdutoDTO {
+    private Long id;
+    private String nome;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
